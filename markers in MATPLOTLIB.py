b3c42b289ce889bcc2c4f568ec7968388ddd8adb
Bharath  Kumar <139763620+Bharathkumaryenumala1@users.noreply.github.com>
2024-09-28 22:59:09 +0530

    Add files via upload
    Actually matplotlib contains N number of Markers in MATPLOTLIB  i used few types in the above repository

---
+++ markers in MATPLOTLIB.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5],"r.")
+plt.xlabel("x_axis")
+plt.ylabel("y_axis")
+plt.show()
+
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5],"rv")
+plt.xlabel("x_axis")
+plt.ylabel("y_axis")
+plt.show()
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5],"bH")
+plt.xlabel("x_axis")
+plt.ylabel("y_axis")
+plt.show()
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4,5],"y*")
+plt.xlabel("x_axis")
+plt.ylabel("y_axis")
+plt.show()
+
